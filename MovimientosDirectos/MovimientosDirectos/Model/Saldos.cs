257a8a7c12cad783089b1753f82592e6a2143a0a
Carlos Alberto <carlosalberto.barrera.contractor@bbva.com>
2023-08-14 11:50:04 -0500

    Progamación base

---
+++ MovimientosDirectos/MovimientosDirectos/Model/Saldos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovimientosDirectos.Model
+{
+    public class Saldos
+    {
+        public string Agencia { get; set; }
+        public string NumCuenta { get; set; }
+        public string SufijoCuenta { get; set; }
+        public decimal SaldoIniDia { get; set; }
+        public int InteresDia { get; set; }
+        public string SwitchBloqueo { get; set; }
+        public DateTime FechaGenSaldo { get; set; }
+        public int NumRegistros { get; set; }
+        public string FinRegistro { get; set; }
+    }
+}
---
+++ MovimientosDirectos/MovimientosDirectos/Model/Vencim.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBVA.MovimientosDirectos.Model
+{
+    public class Vencim
+    {
+        public string Ticket { get; set; }
+        public string Agencia { get; set; }
+        public string NumCuenta { get; set; }
+        public string SufijoCuenta { get; set; }
+        public decimal Saldo { get; set; }
+        public DateTime FechaVen { get; set; }
+        public int NumRegistros { get; set; }
+        public float Intereses { get; set; }
+        public string FinRegistro { get; set; }
+    }
+}
